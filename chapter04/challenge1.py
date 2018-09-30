f76508c2426e26bcdcdb6976add5fe721afd27cd
KurisuLim <kazuki1002003@yahoo.com>
2018-09-30 13:37:34 -0700

    Uploading Chapter 4, challenge 1

---
+++ chapter04/challenge1.py
@@ -0,0 +1,29 @@
+print("""
+Challenge #1
+Write a program that counts for the user. Let the user enter the
+starting number, the ending number, and the amount by which to count.
+""")
+number = 0
+start = None
+finish = None
+
+print("\n\nHello welcome to Challenge 1!\n")
+
+print("Please enter a value between 1 to 10.")
+print("Press the enter key at 'Begin' to exit.")
+
+while start != "":
+    start = input("\nStart: ")
+
+    if start:
+        start = int(start)
+
+        finish = int(input("Finish: "))
+
+        number = finish - start
+
+        print("number[", start, ":", finish, "] is", end=" ")
+        print("total count would be: ", number)
+
+input("\n\nPress the enter key to exit.")
+
